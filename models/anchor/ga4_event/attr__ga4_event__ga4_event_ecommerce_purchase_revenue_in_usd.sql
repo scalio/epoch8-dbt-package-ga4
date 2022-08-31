eddc8fad8364ec16c037da1acf1b1268c1097a39
Sergey Sergey <analytics+dbt-package-ga4@epoch8.co>
2022-08-31 07:20:15 +0000

    Stream_id settings have been implemented.

--- models/anchor/ga4_event/attr__ga4_event__ga4_event_ecommerce_purchase_revenue_in_usd.sql
+++ models/anchor/ga4_event/attr__ga4_event__ga4_event_ecommerce_purchase_revenue_in_usd.sql
@@ -31,6 +31,7 @@ WITH t1 AS (
     WHERE
         _TABLE_SUFFIX NOT LIKE '%intraday%'
         AND PARSE_DATE('%Y%m%d', _TABLE_SUFFIX) > DATE_SUB(DATE(CURRENT_DATE()), INTERVAL {{ var('VAR__DBT_PACKAGE_GA4__INTERVAL') }} DAY)
+        AND events.stream_id IN UNNEST({{ var('VAR__DBT_PACKAGE_GA4__STREAM_ID') }})
     
     {% if is_incremental() %}
     {% set max_patition_date = macro__get_max_patition_date(this.schema, this.table) %}
