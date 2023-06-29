70c5fdb9ba07a3890e21f7ad6b332d04cac20cd2
Sergey Sergey <analytics+dbt-package-ga4@epoch8.co>
2023-06-29 08:29:45 +0000

    Integration layer implemented.

---
+++ models/integration/src_ga4__events.sql
@@ -0,0 +1,18 @@
+{{-
+    config(
+        enabled = env_var('DBT_PACKAGE_GA4__ENABLE__INTEGRATION', 'true') == 'true',
+        tags = ['dbt_package_ga4', 'integration'],
+        materialized = 'view'
+    )
+-}}
+
+
+WITH raw AS (
+    SELECT
+        events._TABLE_SUFFIX AS TABLE_SUFFIX,
+        events.*
+    FROM
+        {{ source('dbt_package_ga4', 'events') }} AS events
+)
+
+SELECT * FROM raw
