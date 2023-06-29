93a0c7d289477e338db3a26b76543ca8aad243d2
Sergey Sergey <analytics+dbt-package-ga4@epoch8.co>
2023-06-29 11:48:42 +0000

    Variable "DBT_PACKAGE_GA4__DATASETS" and more than one dataset might be used now.

--- models/integration/src_ga4__events.sql
+++ models/integration/src_ga4__events.sql
@@ -8,6 +8,11 @@
 
 
 WITH raw AS (
+
+{%- set datasets = env_var('DBT_PACKAGE_GA4__DATASETS', '') -%}
+
+{%- if datasets|length == 0 %}
+
     SELECT
         events._TABLE_SUFFIX AS TABLE_SUFFIX,
         events.*
@@ -15,6 +20,30 @@ WITH raw AS (
         {{ source('dbt_package_ga4', 'events') }} AS events
     WHERE
         events.stream_id IN UNNEST({{ env_var('DBT_PACKAGE_GA4__STREAM_ID') }})
+
+{%- else %}
+
+    {%- set datasets = datasets.split(',') -%}
+
+    {%- for dataset in datasets %}
+
+    SELECT
+        events._TABLE_SUFFIX AS TABLE_SUFFIX,
+        events.*
+    FROM
+        `{{ env_var('DBT_PACKAGE_GA4__PROJECT') }}`.`{{ dataset }}`.`events_*` AS events
+    WHERE
+        events.stream_id IN UNNEST({{ env_var('DBT_PACKAGE_GA4__STREAM_ID') }})
+
+        {%- if not loop.last %}
+
+    UNION ALL
+
+        {%- endif %}
+
+    {%- endfor %}
+
+{%- endif %}
 )
 
 SELECT * FROM raw
